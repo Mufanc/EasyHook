a2a7a67bfd8ecc8ef6a3b11023d100b184335e7b
Mufanc <1714511787@qq.com>
2022-03-07 12:10:07 +0800

    feat: Log.x 支持 vararg 形式的参数

--- app/src/main/java/mufanc/easyhook/tests/ui/MainActivity.kt
+++ app/src/main/java/mufanc/easyhook/tests/ui/MainActivity.kt
@@ -3,12 +3,15 @@ package mufanc.easyhook.tests.ui
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.github.mufanc.easyhook.util.Log
 import mufanc.easyhook.tests.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.i("Test: Log vararg | array", 123456, "string", Any())
     }
 
     override fun onResume() {
--- easyhook/src/main/java/com/github/mufanc/easyhook/util/Log.kt
+++ easyhook/src/main/java/com/github/mufanc/easyhook/util/Log.kt
@@ -12,12 +12,20 @@ object Log {
         Log.v(Globals.TAG, "$message", err)
     }
 
+    fun v(vararg args: Any?) {
+        Log.v(Globals.TAG, args.joinToString(" ") { "$it" })
+    }
+
     fun d(message: Any?, err: Throwable? = null) {
         Log.d(Globals.TAG, "$message", err)
     }
 
     fun d(err: Throwable, message: Any? = "") {
         Log.d(Globals.TAG, "$message", err)
+    }
+
+    fun d(vararg args: Any?) {
+        Log.d(Globals.TAG, args.joinToString(" ") { "$it" })
     }
 
     fun i(message: Any?, err: Throwable? = null) {
@@ -28,6 +36,10 @@ object Log {
         Log.i(Globals.TAG, "$message", err)
     }
 
+    fun i(vararg args: Any?) {
+        Log.i(Globals.TAG, args.joinToString(" ") { "$it" })
+    }
+
     fun w(message: Any?, err: Throwable? = null) {
         Log.w(Globals.TAG, "$message", err)
     }
@@ -36,11 +48,19 @@ object Log {
         Log.w(Globals.TAG, "$message", err)
     }
 
+    fun w(vararg args: Any?) {
+        Log.w(Globals.TAG, args.joinToString(" ") { "$it" })
+    }
+
     fun e(message: Any?, err: Throwable? = null) {
         Log.e(Globals.TAG, "$message", err)
     }
 
     fun e(err: Throwable, message: Any? = "") {
         Log.e(Globals.TAG, "$message", err)
+    }
+
+    fun e(vararg args: Any?) {
+        Log.e(Globals.TAG, args.joinToString(" ") { "$it" })
     }
 }
