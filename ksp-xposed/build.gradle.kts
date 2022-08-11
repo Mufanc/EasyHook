166962cffda05edeb3124471569c7214c9a703ba
Mufanc <1714511787@qq.com>
2022-08-11 19:49:05 +0800

    chore: remove the dependency of `auto-service`

--- ksp-xposed/build.gradle.kts
+++ ksp-xposed/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("java-library")
     id("org.jetbrains.kotlin.jvm")
-    id("com.google.devtools.ksp") version "1.7.10-1.0.6"
 }
 
 val androidSourceCompatibility: JavaVersion by project.parent!!.extra
@@ -16,7 +15,5 @@ dependencies {
     implementation("de.robv.android.xposed:api:82")
 
     compileOnly("com.google.devtools.ksp:symbol-processing-api:1.7.10-1.0.6")
-    implementation("com.google.auto.service:auto-service-annotations:1.0.1")
     implementation("com.squareup:kotlinpoet:1.12.0")
-    ksp("dev.zacsweers.autoservice:auto-service-ksp:1.0.0")
 }
--- ksp-xposed/src/main/java/mufanc/easyhook/ksp_xposed/XposedEntryProcessorProvider.kt
+++ ksp-xposed/src/main/java/mufanc/easyhook/ksp_xposed/XposedEntryProcessorProvider.kt
@@ -1,10 +1,8 @@
 package mufanc.easyhook.ksp_xposed
 
-import com.google.auto.service.AutoService
 import com.google.devtools.ksp.processing.SymbolProcessorEnvironment
 import com.google.devtools.ksp.processing.SymbolProcessorProvider
 
-@AutoService(SymbolProcessorProvider::class)
 class XposedEntryProcessorProvider : SymbolProcessorProvider {
     override fun create(environment: SymbolProcessorEnvironment): XposedEntryProcessor {
         return XposedEntryProcessor(environment)
