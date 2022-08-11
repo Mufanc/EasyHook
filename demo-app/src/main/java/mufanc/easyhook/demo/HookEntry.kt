334dc347b4b127dff5bf81f89a862f7e2c7eaf83
Mufanc <1714511787@qq.com>
2022-08-11 16:57:35 +0800

    feat: update demo

--- demo-app/src/main/java/mufanc/easyhook/demo/HookEntry.kt
+++ demo-app/src/main/java/mufanc/easyhook/demo/HookEntry.kt
@@ -3,12 +3,19 @@ package mufanc.easyhook.demo
 import android.widget.Toast
 import mufanc.easyhook.wrapper.HookHelper
 import mufanc.easyhook.wrapper.annotation.XposedEntry
+import mufanc.easyhook.wrapper.hook
 
 @XposedEntry
 class HookEntry : HookHelper("EasyHook-Demo") {
     override fun onHook() = handle {
         onAttachApplication(BuildConfig.APPLICATION_ID) { lpparam, context ->
-            Toast.makeText(context, lpparam.classLoader.toString(), Toast.LENGTH_SHORT).show()
+            findClass(MainActivity::class.java.name) hook {
+                method({ name == "onCreate" }) {
+                    before {
+                        Toast.makeText(context, lpparam.classLoader.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
         }
     }
 }
