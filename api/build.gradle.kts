f8c19cdc1c123879117e9c397d6b6929a0e241c7
Mufanc <mufanc.xyz@gmail.com>
2022-08-30 22:13:40 +0800

    chore: change dependent type of `androidx.annotation` to `api`

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -41,5 +41,5 @@ android {
 
 dependencies {
     compileOnly("de.robv.android.xposed:api:82")
-    implementation("androidx.annotation:annotation:1.4.0")
+    api("androidx.annotation:annotation:1.4.0")
 }
