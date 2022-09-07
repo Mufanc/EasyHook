bde425cd6ccf8f98348f24b8e849f5b9f7cec548
Mufanc <mufanc.xyz@gmail.com>
2022-09-07 22:27:55 +0800

    feat: update demo

--- demo-app/src/main/java/mufanc/easyhook/demo/HookEntry.kt
+++ demo-app/src/main/java/mufanc/easyhook/demo/HookEntry.kt
@@ -6,6 +6,7 @@ import mufanc.easyhook.api.annotation.XposedEntry
 import mufanc.easyhook.api.hook.HookHelper
 import mufanc.easyhook.api.hook.hook
 import mufanc.easyhook.api.reflect.findMethod
+import java.lang.reflect.Method
 
 @XposedEntry
 class HookEntry : HookHelper("EasyHook-Demo") {
@@ -15,15 +16,30 @@ class HookEntry : HookHelper("EasyHook-Demo") {
             findClass(MainActivity::class.java.name).hook { clazz ->
                 method({ name == "onCreate" }) {
                     before {
-                        Toast.makeText(context, clazz.toString(), Toast.LENGTH_SHORT).show()
+                        Toast.makeText(context, clazz.signature(), Toast.LENGTH_SHORT).show()
                     }
                 }
                 clazz.findMethod { name == "onCreate" }!!.hook { method ->
                     after {
-                        Toast.makeText(context, method.toString(), Toast.LENGTH_SHORT).show()
+                        Toast.makeText(context, method.signature(), Toast.LENGTH_SHORT).show()
                     }
                 }
             }
         }
+    }
+
+    private fun Class<*>.signature(): String {
+        return java.lang.reflect.Array.newInstance(this, 0)
+            .javaClass.name.replace('.', '/').substring(1)
+    }
+
+    private fun Method.signature(): String {
+        val builder = StringBuilder("(")
+        this.parameterTypes.forEach {
+            builder.append(it.signature())
+        }
+        builder.append(")")
+        builder.append(if (returnType == Void.TYPE) "V" else returnType.signature())
+        return builder.toString()
     }
 }
