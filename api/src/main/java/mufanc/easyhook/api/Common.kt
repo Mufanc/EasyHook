ba70a2f4532906520aea9f802e885788a2cdd1a9
Mufanc <mufanc.xyz@gmail.com>
2022-09-15 21:34:45 +0800

    feat: `Class<*>.signature()` and `Method.signature()`

--- api/src/main/java/mufanc/easyhook/api/Common.kt
+++ api/src/main/java/mufanc/easyhook/api/Common.kt
@@ -1,9 +1,26 @@
 package mufanc.easyhook.api
 
+import java.lang.reflect.Method
+
 inline fun catch(block: () -> Unit) {
     try {
         block()
     } catch (err: Throwable) {
         Logger.e(err = err)
     }
+}
+
+fun Class<*>.signature(): String {
+    return java.lang.reflect.Array.newInstance(this, 0)
+        .javaClass.name.replace('.', '/').substring(1)
+}
+
+fun Method.signature(): String {
+    val builder = StringBuilder("(")
+    this.parameterTypes.forEach {
+        builder.append(it.signature())
+    }
+    builder.append(")")
+    builder.append(if (returnType == Void.TYPE) "V" else returnType.signature())
+    return builder.toString()
 }
